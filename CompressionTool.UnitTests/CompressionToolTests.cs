a43df9662239fe17c53d16bc2233f461751f5a24
Reymon Reyes <elmoe01@gmail.com>
2025-04-03 18:16:42 +0800

    3 - Step 1 Count the frequency of each character.

--- CompressionTool.UnitTests/CompressionToolTests.cs
+++ CompressionTool.UnitTests/CompressionToolTests.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace CompressionTool.UnitTests
 {
-    internal class CompressionToolTests
+    public class CompressionToolTests
     {
+        [Fact]
+        public void Compress_ShouldReturnCorrectCounts()
+        {
+            var compression = new CCCompressionTool();
+            var text = "In this step your goal is to build your tool to accept a filename as input. It should return an error if the file is not valid, otherwise your program should open it, read the text and determine the frequency of each character occurring within the text.";
+
+            var results = compression.Compress(text);
+
+            Assert.Contains(results, x => x.Key == 'x' && x.Value == 2);
+            Assert.Contains(results, x => x.Key == 'o' && x.Value == 17);
+            Assert.Contains(results, x => x.Key == 'e' && x.Value == 24);
+        }
     }
 }
---
+++ CompressionTool/CCCompressionTool.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompressionTool
+{
+    public class CCCompressionTool
+    {
+        public Dictionary<char, int> Compress(string text)
+        {
+            var result = new Dictionary<char, int>();
+
+            foreach (var character in text)
+            {
+                if(!result.ContainsKey(character))
+                {
+                    result.Add(character, 1);
+                }
+                else
+                {
+                    result[character]++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
