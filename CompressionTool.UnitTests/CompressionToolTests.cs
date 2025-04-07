c1f4811305347170712f0f3f002bd5f9c9c2525c
Reymon Reyes <elmoe01@gmail.com>
2025-04-07 18:30:49 +0800

    3 - Step 1 A separate method to build frequency table.

--- CompressionTool.UnitTests/CompressionToolTests.cs
+++ CompressionTool.UnitTests/CompressionToolTests.cs
@@ -9,16 +9,18 @@ namespace CompressionTool.UnitTests
     public class CompressionToolTests
     {
         [Fact]
-        public void Compress_ShouldReturnCorrectCounts()
+        public void BuildFrequencyTable_ShouldReturnCorrectCounts()
         {
             var compression = new CCCompressionTool();
-            var text = "In this step your goal is to build your tool to accept a filename as input. It should return an error if the file is not valid, otherwise your program should open it, read the text and determine the frequency of each character occurring within the text.";
+            var text = "aaaaaasssssssssdddddddddddffffffffffffffffffggggggggggggggggggggghhh";
 
-            var results = compression.Compress(text);
+            var results = compression.BuildFrequencyTable(text);
 
-            Assert.Contains(results, x => x.Key == 'x' && x.Value == 2);
-            Assert.Contains(results, x => x.Key == 'o' && x.Value == 17);
-            Assert.Contains(results, x => x.Key == 'e' && x.Value == 24);
-        }
+            Assert.Contains(results, x => x.Key == 'a' && x.Value == 6);
+            Assert.Contains(results, x => x.Key == 's' && x.Value == 9);
+            Assert.Contains(results, x => x.Key == 'h' && x.Value == 3);
+            Assert.True(results.FirstOrDefault().Key == 'h');
+            Assert.True(results.LastOrDefault().Key == 'g');
+        }        
     }
 }
--- CompressionTool/CCCompressionTool.cs
+++ CompressionTool/CCCompressionTool.cs
@@ -10,11 +10,16 @@ namespace CompressionTool
     {
         public Dictionary<char, int> Compress(string text)
         {
+            return new Dictionary<char, int>();
+        }
+
+        public Dictionary<char, int> BuildFrequencyTable(string text)
+        {
             var result = new Dictionary<char, int>();
 
             foreach (var character in text)
             {
-                if(!result.ContainsKey(character))
+                if (!result.ContainsKey(character))
                 {
                     result.Add(character, 1);
                 }
@@ -24,7 +29,7 @@ namespace CompressionTool
                 }
             }
 
-            return result;
+            return result.OrderBy(x => x.Value).ToDictionary();
         }
     }
 }
