ea7d0bbe9c44ea9e45aff2de144bfc5507e34d7d
Reymon Reyes <elmoe01@gmail.com>
2026-01-27 15:31:34 +0800

    Step 3 Option to pass a field list separated by comma.

--- cutTool.UnitTests/CutToolTests.cs
+++ cutTool.UnitTests/CutToolTests.cs
@@ -11,13 +11,13 @@ namespace cutTool.UnitTests
         [Fact]
         public void CutField_ShouldReturnValuesOfSpecifiedField()
         {
-            string[] expected = ["f1", "1", "6", "11", "16", "21"];
+            string[,] expected = { { "f1" }, { "1" }, { "6" }, { "11" }, { "16" }, { "21" } };
             var cutTool = new Cut();
 
             var result = cutTool.Run(["f2"], "./challenge-cut/sample.tsv");
             Assert.Equal(expected, result);
 
-            string[] expected2 = ["f0", "0", "5", "10", "15", "20"];
+            string[,] expected2 = { { "f0" }, { "0" }, { "5" }, { "10" }, { "15" }, { "20" } };
             var result2 = cutTool.Run(["f1"], "./challenge-cut/sample.tsv");
             Assert.Equal(expected2, result2);
         }
@@ -25,12 +25,33 @@ namespace cutTool.UnitTests
         [Fact]
         public void CutWithDelimiter_ShouldReturnValuesFromSpecifiedDelimiter()
         {
-            string[] expected = ["Song title", "\"10000 Reasons (Bless the Lord)\"", "\"20 Good Reasons\"", "\"Adore You\"", "\"Africa\""];
+            string[,] expected = { { "Song title" }, { "\"10000 Reasons (Bless the Lord)\"" }, { "\"20 Good Reasons\"" }, { "\"Adore You\"" }, {"\"Africa\"" } };
             var cutTool = new Cut();
-            var result = cutTool.Run(["f1", "d,"], "./challenge-cut/fourchords.csv");
-            result = result.Take(5).ToArray();
+            var runResult = cutTool.Run(["f1", "d,"], "./challenge-cut/fourchords.csv");
+            string[,] result = new string[5, 1];
+
+            for (int i = 0; i < 5; i++)
+                result[i, 0] = runResult[i, 0];
 
             Assert.Equal(expected, result);
         }
+
+        [Fact]
+        public void CutWithMultipleFields_CommaSpecifiedFieldsShouldReturnDataFromSetFields()
+        {
+            string[,] expected = {
+                {"f0", "f1"},
+                {"0", "1"},
+                {"5", "6"},
+                {"10", "11"},
+                {"15", "16"},
+                {"20", "21"},
+            };
+
+            var cutTool = new Cut();
+            var result = cutTool.Run(["f1,2"], "./challenge-cut/sample.tsv");
+
+            Assert.Equal(expected, result);
+        }        
     }
 }
--- cutTool/Cut.cs
+++ cutTool/Cut.cs
@@ -8,40 +8,71 @@ namespace cutTool
 {
     public class Cut
     {
-        public string[] Run(string[] options, string filePath)
+        public string[,] Run(string[] options, string filePath)
         {
             if (options.Length == 0 || string.IsNullOrWhiteSpace(filePath))
-                return new string[0];
-            
-            //make sure only valid options: f(field),d(delimiter)
-            if(options.Where(x => x.StartsWith("f") || x.StartsWith("d")).Count() == 0)
-                return new string[0];
+                return new string[0, 0];
 
-            var file = File.ReadAllLines(filePath);
-            string[] result = new string[file.Length];
+            //make sure only valid options: f(field),d(delimiter)
+            if (options.Where(x => x.StartsWith("f") || x.StartsWith("d")).Count() == 0)
+                return new string[0, 0];           
 
             //get field param
             var fieldParam = options.FirstOrDefault(x => x.StartsWith("f"));
-            if(string.IsNullOrEmpty(fieldParam) || fieldParam.Length > 2)
-                return new string[0];
+            if (string.IsNullOrEmpty(fieldParam) || fieldParam.Length < 2)
+                return new string[0, 0];
+            
+            var file = File.ReadAllLines(filePath);
 
-            if (!int.TryParse(fieldParam[1].ToString(), out int fieldIndex))
-                return new string[0];
+            var fields = fieldParam.Substring(1).Split(",");
 
-            if (fieldIndex <= 0)
-                return new string[0];
+            if (fields.Length == 0)
+                return new string[0, 0];
 
+            if (fields.Any(x => !int.TryParse(x, out int fieldIndex)))
+                return new string[0, 0];            
+
+            //only valid fields
+            var fieldIndexes = new List<int>();
+            var hasInvalidField = false;
+            foreach ( var field in fields)
+            {
+                if (int.TryParse(field, out var fieldIndex))
+                {
+                    if(fieldIndex <= 0)
+                    {
+                        hasInvalidField = true;
+                        break;
+                    }
+
+                    if (fieldIndex > 0)
+                        fieldIndexes.Add(fieldIndex-1);//note: field position number is 1 based
+                }
+                else
+                {
+                    hasInvalidField = true;
+                    break;
+                }
+            }
+
+            if(hasInvalidField) return new string[0, 0];
+
+            var result = new string[file.Length, fieldIndexes.Count];
             //get delimiter param
             var delimiter = '\t';//default
             var delimiterParam = options.FirstOrDefault(x => x.StartsWith("d"));
             if(!string.IsNullOrWhiteSpace(delimiterParam) && delimiterParam.Length == 2)
                 delimiter = delimiterParam[1];
 
-            for (int i = 0; i < result.Length; i++)
+            for (int i = 0; i < file.Length; i++)
             {
                 var line = file[i];
                 var rowFields = line.Split(delimiter);
-                result[i] = rowFields[fieldIndex - 1];//note: field position number is 1 based
+
+                for (int x = 0; x < fieldIndexes.Count; x++)
+                {
+                    result[i,x] = rowFields[fieldIndexes[x]];
+                }
             }
 
             return result;
