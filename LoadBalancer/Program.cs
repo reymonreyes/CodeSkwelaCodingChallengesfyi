79b74beb1de09a250cbab30bef24722955ca8a5f
Reymon Reyes <elmoe01@gmail.com>
2026-01-31 19:30:25 +0800

    Step 1 Listen for a request and forward to a single server.
    - My first option was to use TcpListener but it's more low level and don't want to waste time on it since I just want to narrow it down to http request. I found HttpListener much easier to use and so I can focus on the load balancer algorithm.
    - Use curl to make a request. I also use Talend API (Edge browser extension) tester as an alternative.
    - Used HttpClient class to make a call to backend server.

---
+++ LoadBalancer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+using System.Text;
+using System.Text.Unicode;
+
+namespace LoadBalancer
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //client -> load balancer -> backend server -> load balancer -> client
+
+            var httpListener = new HttpListener();
+            httpListener.Prefixes.Add("http://localhost:8080/");
+
+            while (true)
+            {
+                httpListener.Start();
+                Console.WriteLine("Listening for requests...");
+                var context = await httpListener.GetContextAsync();
+                var request = context.Request;
+
+                //return some info
+                Console.WriteLine($"Request received from {request.RemoteEndPoint}");
+                Console.WriteLine($"{request.HttpMethod} / HTTP/{request.ProtocolVersion.Major}.{request.ProtocolVersion.Minor}");
+                Console.WriteLine($"Host: {request.Url.Host}");
+                Console.WriteLine($"User-Agent: {request.UserAgent}");
+                Console.WriteLine($"Accept: {(request.AcceptTypes != null ? string.Join(',', request.AcceptTypes) : "")}");
+
+                //make a request to backend server
+                var httpClient = new HttpClient();
+                var backendRequest = await httpClient.GetAsync("http://localhost:8081/");
+                var brResponse = await backendRequest.Content.ReadAsStringAsync();
+
+                //show the response
+                Console.Write(brResponse);
+
+                //return the response to client
+                var buffer = Encoding.UTF8.GetBytes(brResponse);
+                var response = context.Response;
+                response.ContentLength64 = buffer.Length;
+                Stream output = response.OutputStream;
+                await output.WriteAsync(buffer, 0, buffer.Length);
+                output.Close();
+
+                httpListener.Stop();
+            }
+        }
+    }
+}
