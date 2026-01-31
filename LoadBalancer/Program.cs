cfd338ac96c28d3a9c431ab839ec0428107d1523
Reymon Reyes <elmoe01@gmail.com>
2026-01-31 21:22:00 +0800

    Step 2 Distribute request by using a round robin schedule.

--- LoadBalancer/Program.cs
+++ LoadBalancer/Program.cs
@@ -13,14 +13,18 @@ namespace LoadBalancer
             var httpListener = new HttpListener();
             httpListener.Prefixes.Add("http://localhost:8080/");
 
+            httpListener.Start();
+            var nextBackendServer = 0;
+            var backendServersUrls = new string[]{ "http://localhost:8081", "http://localhost:8082", "http://localhost:8083" };
+
             while (true)
             {
-                httpListener.Start();
                 Console.WriteLine("Listening for requests...");
                 var context = await httpListener.GetContextAsync();
                 var request = context.Request;
 
                 //return some info
+                Console.WriteLine($"[{DateTime.Now.ToString("O")}]");
                 Console.WriteLine($"Request received from {request.RemoteEndPoint}");
                 Console.WriteLine($"{request.HttpMethod} / HTTP/{request.ProtocolVersion.Major}.{request.ProtocolVersion.Minor}");
                 Console.WriteLine($"Host: {request.Url.Host}");
@@ -28,12 +32,16 @@ namespace LoadBalancer
                 Console.WriteLine($"Accept: {(request.AcceptTypes != null ? string.Join(',', request.AcceptTypes) : "")}");
 
                 //make a request to backend server
+                var backendServerUrl = backendServersUrls[nextBackendServer % backendServersUrls.Length];
+                Console.WriteLine($"Backend request to server {backendServerUrl}\n");
+                nextBackendServer++;
+
                 var httpClient = new HttpClient();
-                var backendRequest = await httpClient.GetAsync("http://localhost:8081/");
+                var backendRequest = await httpClient.GetAsync(backendServerUrl);
                 var brResponse = await backendRequest.Content.ReadAsStringAsync();
 
                 //show the response
-                Console.Write(brResponse);
+                //Console.Write(brResponse);
 
                 //return the response to client
                 var buffer = Encoding.UTF8.GetBytes(brResponse);
@@ -43,8 +51,10 @@ namespace LoadBalancer
                 await output.WriteAsync(buffer, 0, buffer.Length);
                 output.Close();
 
-                httpListener.Stop();
+                Console.WriteLine();
             }
+
+            httpListener.Stop();
         }
     }
 }
